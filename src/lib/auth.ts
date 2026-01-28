42d879200ca24357b480c9a17ce48e5eca9ae33b
asadalgalib <asadalgalib9@gmail.com>
2026-01-28 20:54:26 +0600

    better auth installed and generated user schema

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    trustedOrigins: [`${process.env.APP_URL}`, "http://localhost:5000"],
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+    },
+});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`app is running on ${port}`);
-})
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("database connected");
+
+        app.listen(port, () => {
+            console.log(`app is running on ${port}`);
+        })
+    } catch (error: any) {
+        console.error(error.message)
+
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+main();
